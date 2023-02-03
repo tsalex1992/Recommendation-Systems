e1b7a3c7309b1e010317d33d1b75196a98f2a9f2
tsalex1992 <tsalex1992@gmail.com>
2023-02-03 14:44:57 +0200

    Added support for unix time

--- Project/CB2CFTransformer.py
+++ Project/CB2CFTransformer.py
@@ -18,7 +18,7 @@ class CB2CFMultiModalEncoder(nn.Module):
         self.genres_embedding = nn.Embedding(number_of_genres, item_embedding_dim)
         self.actors_embedding = nn.Embedding(number_of_actors, item_embedding_dim)
         self.directors_embedding = nn.Embedding(number_of_directors, item_embedding_dim)
-        self.year_layer = nn.Sequential(
+        self.time_layer = nn.Sequential(
             nn.Linear(1, item_embedding_dim),
             nn.ReLU(),
             nn.Dropout(0.2),
@@ -38,14 +38,16 @@ class CB2CFMultiModalEncoder(nn.Module):
         for param in self.text_encoding_model.parameters():
             param.requires_grad = False
 
-    def forward(self, genres, actors, directors, year, description, language):
+    def forward(self, genres, actors, directors, unix_release_time, description, language):
         genres = self.genres_embedding(genres)
         actors = self.actors_embedding(actors)
         directors = self.directors_embedding(directors)
-        year = self.year_layer(year)
+        unix_release_time = self.time_layer(unix_release_time)
         language = self.language_embedding(language)
         description = self.text_encoding_model.encode(description)
         description = self.description_layer(description)
-        movie_embedding = torch.cat((genres, actors, directors, year, description, language), dim=1)
+        movie_embedding = torch.cat(
+            (genres, actors, directors, unix_release_time, description, language), dim=1
+        )
         movie_embedding = self.fc_layer(movie_embedding)
         return movie_embedding
---
+++ Project/NextItemPredTransformer.py
@@ -0,0 +1,241 @@
+from dataclasses import dataclass
+from typing import Dict
+from typing import Iterable, Optional
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torch import Tensor
+from torch import nn
+
+
+@dataclass
+class ModelDimensions:
+    pre_trained_item_embeddings: Tensor
+    n_item_ctx: int
+    n_item_state: int
+    n_item_head: int
+    n_item_layer: int
+    pre_trained_user_embeddings: Optional[Tensor]
+
+
+class LayerNorm(nn.LayerNorm):
+    def forward(self, x: Tensor) -> Tensor:
+        return super().forward(x.float()).type(x.dtype)
+
+
+class Linear(nn.Linear):
+    def forward(self, x: Tensor) -> Tensor:
+        return F.linear(
+            x, self.weight.to(x.dtype), None if self.bias is None else self.bias.to(x.dtype)
+        )
+
+
+class Conv1d(nn.Conv1d):
+    def _conv_forward(self, x: Tensor, weight: Tensor, bias: Optional[Tensor]) -> Tensor:
+        return super()._conv_forward(
+            x, weight.to(x.dtype), None if bias is None else bias.to(x.dtype)
+        )
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, n_state: int, n_head: int):
+        """Multi-head attention layer
+
+        Args:
+            n_state: number of hidden units
+            n_head: number of attention heads
+        """
+        super().__init__()
+        self.n_head = n_head
+        self.query = Linear(n_state, n_state)
+        self.key = Linear(n_state, n_state, bias=False)
+        self.value = Linear(n_state, n_state)
+        self.out = Linear(n_state, n_state)
+
+    def forward(
+        self,
+        x: Tensor,
+        mask: Optional[Tensor] = None,
+        kv_cache: Optional[dict] = None,
+    ):
+        q = self.query(x)
+
+        if kv_cache is None or self.key not in kv_cache:
+            # hooks, if installed (i.e. kv_cache is not None), will prepend the cached kv tensors;
+            # otherwise, perform key/value projections for self- or cross-attention as usual.
+            k = self.key(x)
+            v = self.value(x)
+        else:
+            # for cross-attention, calculate keys and values once and reuse in subsequent calls.
+            k = kv_cache[self.key]
+            v = kv_cache[self.value]
+
+        wv, qk = self.qkv_attention(q, k, v, mask)
+        return self.out(wv), qk
+
+    def qkv_attention(self, q: Tensor, k: Tensor, v: Tensor, mask: Optional[Tensor] = None):
+        n_batch, n_ctx, n_state = q.shape
+        scale = (n_state // self.n_head) ** -0.25
+        # Output dimensions: [n_batch, n_ctx, n_head, n_head_dim]
+        q = q.view(*q.shape[:2], self.n_head, -1).permute(0, 2, 1, 3) * scale
+        k = k.view(*k.shape[:2], self.n_head, -1).permute(0, 2, 3, 1) * scale
+        v = v.view(*v.shape[:2], self.n_head, -1).permute(0, 2, 1, 3)
+
+        qk = q @ k
+        if mask is not None:
+            qk = qk + mask[:n_ctx, :n_ctx]
+        qk = qk.float()
+
+        w = F.softmax(qk, dim=-1).to(q.dtype)
+        return (w @ v).permute(0, 2, 1, 3).flatten(start_dim=2), qk.detach()
+
+
+class ResidualAttentionBlock(nn.Module):
+    def __init__(self, n_state: int, n_head: int):
+        super().__init__()
+
+        self.attn = MultiHeadAttention(n_state, n_head)
+        self.attn_ln = LayerNorm(n_state)
+
+        n_mlp = n_state * 4
+        self.mlp = nn.Sequential(Linear(n_state, n_mlp), nn.GELU(), Linear(n_mlp, n_state))
+        self.mlp_ln = LayerNorm(n_state)
+
+    def forward(
+        self,
+        x: Tensor,
+        mask: Optional[Tensor] = None,
+        kv_cache: Optional[dict] = None,
+    ):
+        x = x + self.attn(self.attn_ln(x), mask=mask, kv_cache=kv_cache)[0]
+        x = x + self.mlp(self.mlp_ln(x))
+        return x
+
+
+class ItemDecoder(nn.Module):
+    def __init__(
+        self,
+        pre_trained_item_embeddings: Tensor,
+        n_ctx: int,
+        n_state: int,
+        n_head: int,
+        n_layer: int,
+        pre_trained_user_embeddings: Optional[Tensor] = None,
+    ):
+        """Item decoder
+
+        Args:
+            pre_trained_item_embeddings: pre-trained item embeddings, of shape [n_vocab, n_state] ordered by item id
+            n_ctx: number of items in context
+            n_state: number of hidden units
+            n_head: number of attention heads
+            n_layer: number of layers
+        """
+        super().__init__()
+
+        self.items_embedding = nn.Embedding.from_pretrained(pre_trained_item_embeddings)
+        self.users_embedding = (
+            nn.Embedding.from_pretrained(pre_trained_user_embeddings)
+            if pre_trained_user_embeddings is not None
+            else None
+        )
+        self.positional_embedding = nn.Parameter(torch.empty(n_ctx, n_state))
+        self.time_embedding = nn.Sequential(nn.Linear(1, n_state), nn.ReLU())
+
+        self.blocks: Iterable[ResidualAttentionBlock] = nn.ModuleList(
+            [ResidualAttentionBlock(n_state, n_head) for _ in range(n_layer)]
+        )
+        self.ln = LayerNorm(n_state)
+
+        mask = torch.empty(n_ctx, n_ctx).fill_(-np.inf).triu_(1)
+        self.register_buffer("mask", mask, persistent=False)
+
+    def forward(
+        self,
+        items: Tensor,
+        user: Optional[Tensor],
+        kv_cache: Optional[dict] = None,
+        time: Optional[Tensor] = None,
+    ):
+        """
+        items : torch.LongTensor, shape = (batch_size, <= n_ctx)
+            the text tokens
+        user : torch.LongTensor, shape = (batch_size,)
+        """
+        offset = next(iter(kv_cache.values())).shape[1] if kv_cache else 0
+        items = (
+            self.items_embedding(items)
+            + self.positional_embedding[offset : offset + items.shape[-1]]
+        )
+
+        if self.users_embedding is not None:
+            items = items + self.users_embedding(user)
+
+        if time is not None:
+            items = items + self.time_embedding(time)
+
+        for block in self.blocks:
+            items = block(items, mask=self.mask, kv_cache=kv_cache)
+
+        items = self.ln(items)
+        logits = (
+            items @ torch.transpose(self.items_embedding.weight.to(items.dtype), 0, 1)
+        ).float()
+
+        return logits
+
+
+class NextItemPredTransformer(nn.Module):
+    def __init__(self, dims: ModelDimensions):
+        super().__init__()
+        self.dims = dims
+        self.decoder = ItemDecoder(
+            self.dims.pre_trained_item_embeddings,
+            self.dims.n_item_ctx,
+            self.dims.n_item_state,
+            self.dims.n_item_head,
+            self.dims.n_item_layer,
+            self.dims.pre_trained_user_embeddings,
+        )
+
+    def logits(self, tokens: torch.Tensor):
+        return self.decoder(tokens)
+
+    def forward(self, tokens: torch.Tensor) -> Dict[str, torch.Tensor]:
+        return self.decoder(tokens)
+
+    @property
+    def device(self):
+        return next(self.parameters()).device
+
+    def install_kv_cache_hooks(self, cache: Optional[dict] = None):
+        """
+        The `MultiHeadAttention` module optionally accepts `kv_cache` which stores the key and value
+        tensors calculated for the previous positions. This method returns a dictionary that stores
+        all caches, and the necessary hooks for the key and value projection modules that save the
+        intermediate tensors to be reused during later calculations.
+        Returns
+        -------
+        cache : Dict[nn.Module, torch.Tensor]
+            A dictionary object mapping the key/value projection modules to its cache
+        hooks : List[RemovableHandle]
+            List of PyTorch RemovableHandle objects to stop the hooks to be called
+        """
+        cache = {**cache} if cache is not None else {}
+        hooks = []
+
+        def save_to_cache(module, _, output):
+            if module not in cache or output.shape[1] > self.decoder.positional_embedding.shape[0]:
+                cache[module] = output  # save as-is, for the first token or cross attention
+            else:
+                cache[module] = torch.cat([cache[module], output], dim=1).detach()
+            return cache[module]
+
+        def install_hooks(layer: nn.Module):
+            if isinstance(layer, MultiHeadAttention):
+                hooks.append(layer.key.register_forward_hook(save_to_cache))
+                hooks.append(layer.value.register_forward_hook(save_to_cache))
+
+        self.decoder.apply(install_hooks)
+        return cache, hooks
